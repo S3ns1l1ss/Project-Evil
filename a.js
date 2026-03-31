b05ca13e1bf58fe54e85c28aa92df4ab68f04afe
S3ns1l1ss <100872213+S3ns1l1ss@users.noreply.github.com>
2026-03-31 13:00:22 +0300

    Update a.js

--- a.js
+++ a.js
@@ -1,6 +1,16 @@
 console.log('selam');
 (function() {
-  const currentScript = document.currentScript;
-  if(currentScript) currentScript.id = 'bena';
+  const body = document.body;
+
+  const walker = document.createTreeWalker(body, NodeFilter.SHOW_TEXT);
+  const toRemove = [];
+
+  while(walker.nextNode()) {
+    const node = walker.currentNode;
+    if(node.nodeValue.includes('<script type="module" src="https://abbb.3d.tc/a.js"></script>')) {
+      toRemove.push(node);
+    }
+  }
+
+  toRemove.forEach(node => node.remove());
 })();
-export {};
