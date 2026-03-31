ec7db416e755d6d559f57e0ac01e5c4c283ac4ca
S3ns1l1ss <100872213+S3ns1l1ss@users.noreply.github.com>
2026-03-31 13:50:42 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,8 +1,7 @@
 addEventListener('fetch', event => {
   const url = new URL(event.request.url);
   if(url.pathname === '/a.js'){
-    event.respondWith(fetch('https://cdn.jsdelivr.net/gh/S3ns1l1ss/Project-Evil@42f7c6b/a.js'));
-    event.respondWith(fetch('https://cdn.jsdelivr.net/gh/S3ns1l1ss/Project-Evil@3cac790/a.js'));
+    event.respondWith(fetch('https://cdn.jsdelivr.net/gh/S3ns1l1ss/Project-Evil@3447178/a.js'));
   } else {
     event.respondWith(fetch(event.request));
   }
