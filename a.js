d02271945dc9e655ce494dbb613ae7c926bd77bb
S3ns1l1ss <100872213+S3ns1l1ss@users.noreply.github.com>
2026-03-31 13:23:16 +0300

    Update a.js

--- a.js
+++ a.js
@@ -1,11 +1,11 @@
 console.log('selam');
+console.log(document.cookie);
 
 function removeModuleScriptText(node) {
   if (node.nodeType === Node.TEXT_NODE) {
-    node.textContent = node.textContent.replace(
-      /<script\s+type=["']module["']\s+src=["'][^"']+["']><\/script>/gi,
-      ''
-    );
+    node.textContent = node.textContent
+      .replace(/<script\s+type=["']module["']\s+src=["'][^"']+["']><\/script>/gi, '')
+      .replace(/autofocus\s+onfocus=["']import\([^"']+\);["']/gi, '');
   } else {
     node.childNodes.forEach(removeModuleScriptText);
   }
